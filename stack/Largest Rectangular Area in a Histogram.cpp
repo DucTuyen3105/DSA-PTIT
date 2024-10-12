b7e59cfad74ca0079bcc6aeb86a9982c56e77f37
DucTuyen3105 <ductuyenn15@gmail.com>
2024-10-13 03:10:44 +0700

    Update Largest Rectangular Area in a Histogram.cpp

--- stack/Largest Rectangular Area in a Histogram.cpp
+++ stack/Largest Rectangular Area in a Histogram.cpp
@@ -1,1 +1,55 @@
 #include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	int n;
+	cin>>n;
+	ll a[n];
+	for(int i = 0 ; i < n ; i++)
+	{
+		cin>>a[i];
+	}
+	ll left[n];
+	stack<ll>st;
+	for(int i = n - 1 ; i >= 0 ; i--)
+	{
+		while(!st.empty() && a[st.top()] > a[i])
+		{
+			left[st.top()] = i; st.pop();
+		}
+		st.push(i);
+	}
+	while(!st.empty())
+	{
+		left[st.top()] = -1;
+		st.pop();
+	}
+	ll right[n];
+	for(int i = 0 ; i < n ; i++)
+	{
+		while(!st.empty() && a[st.top()] > a[i])
+		{
+			right[st.top()] = i; st.pop();
+		}
+		st.push(i);
+	}
+	while(!st.empty()) 
+	{
+		right[st.top()] = n; st.pop();
+	}
+	ll ans = LLONG_MIN;
+	for(int i = 0 ; i < n ; i++)
+	{
+		ll temp = 1ll*a[i] * (right[i] - left[i] - 1);
+		ans = max(ans,temp);
+	}
+	cout<<ans;	
+	cout<<endl;
+	}
+}
+
